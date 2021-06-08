e78743111ccdbab80cbd8547a27de0148fa575d3
Austin Rohn <Austinrohn19@gmail.com>
2021-06-07 21:53:51 -0400

    hu

---
+++ homework2/assets/javascript.js
@@ -0,0 +1,52 @@
+
+document.getElementById("button1").addEventListener("click", function(){
+   
+    document.getElementById("box").style.height ="250px"; 
+});
+document.getElementById("button1").addEventListener("click", function(){
+   
+   document.getElementById("box").style.backgroundColor ="orange"; 
+});
+document.getElementById("button1").addEventListener("click", function(){
+
+document.getElementById("box").style.opacity = "100%"
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+   document.getElementById("box").style.backgroundColor ="blue"; 
+});
+document.getElementById("button2").addEventListener("click", function(){
+
+document.getElementById("box").style.opacity = "100%"; 
+});
+document.getElementById("button2").addEventListener("click", function(){
+   
+   document.getElementById("box").style.height ="150px"; 
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+   
+   document.getElementById("box").style.opacity = "50%"; 
+});
+document.getElementById("button3").addEventListener("click", function(){
+   
+   document.getElementById("box").style.height ="150px"; 
+});
+document.getElementById("button3").addEventListener("click", function(){
+   
+   document.getElementById("box").style.backgroundColor ="orange"; 
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+   
+   document.getElementById("box").style.backgroundColor = "orange";
+});
+document.getElementById("button4").addEventListener("click", function(){
+   
+   document.getElementById("box").style.height ="150px";
+});
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.opacity = "100%"
+});
